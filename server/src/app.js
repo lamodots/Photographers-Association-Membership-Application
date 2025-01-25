e2336c2dc18ae3c27703eb2c346af29738329e68
lamodots <lamodots@gmail.com>
2025-01-25 12:49:12 -0800

    refacted app.js to fix upload of logo not showing

--- server/src/app.js
+++ server/src/app.js
@@ -45,11 +45,19 @@ app.use("/api/v1/secure/events", eventRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "client", "public", "uploads"))
+  );
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client/build/index.html"))
   );
 } else {
+  app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "client", "public", "uploads"))
+  );
   app.get("/", (req, res) => {
     res.json({ message: "Welcome to the MembershipCMS backend API" });
   });
