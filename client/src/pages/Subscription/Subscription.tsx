b3005c670bad57a9a38ff64671b3a861a213c418
lamodots <lamodots@gmail.com>
2025-04-16 09:05:58 -0700

    Fix: Life membership and Honorarry member should not see Membership Dues payment

--- client/src/pages/Subscription/Subscription.tsx
+++ client/src/pages/Subscription/Subscription.tsx
@@ -1,5 +1,5 @@
 import { PhoneCallIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Lable from "../../components/Lable/Lable";
 import Button from "../../components/Button/Button";
 
@@ -22,7 +22,12 @@ interface PaymentProps {
 }
 
 function Subscription() {
-  const { currentUser } = useCurrentUser();
+  const {
+    currentUser,
+    loading: currentUserLoading,
+    fetchCurrentUser,
+  } = useCurrentUser();
+  console.log(currentUser?.user.membershipType);
   const { appData, loading, error: settingError } = useFetchAppData();
   const [membershipItem] = useMembershipActive();
   const [showManualPayment, setShowManualPayment] = useState(false);
@@ -35,6 +40,11 @@ function Subscription() {
   const [error, setError] = useState({ valueError: "" });
   const navigate = useNavigate();
   console.log("app setttings", appData);
+  useEffect(() => {
+    if (!currentUser) {
+      fetchCurrentUser(); // Ensure currentUser is always fetched
+    }
+  }, [currentUser?.user, fetchCurrentUser]);
   const handleShowPaymentType = () => {
     setShowManualPayment(true);
     setShowManualPaymentOnline(false);
@@ -141,6 +151,10 @@ function Subscription() {
       setIsSubmittingWelfarePayment(false);
     }
   };
+
+  if (currentUserLoading || currentUser?.user == undefined) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <nav>
@@ -190,74 +204,80 @@ function Subscription() {
             })}
         </div>
       )}
+      <>
+        {currentUser?.user.membershipType !== "Honourary membership" && (
+          <p>Play</p>
+        )}
+      </>
       {showManualPaymentOnline && (
         <div className="space-y-4">
-          {(membershipItem?.membershipType === undefined ||
-            (membershipItem?.membershipType !== "Life membership" &&
-              membershipItem?.membershipType !== "Honorary member")) && (
-            <div className=" p-6 bg-white rounded-md shadow-sm transition-all max-w-[512px] mt-6">
-              <h3 className="text-lg font-bold">
-                <strong>Membership Dues</strong>, Pay online
-              </h3>
-              <p className="mb-6">
-                Pay for your membership fee online secure and fast
-              </p>
-              <div className="  md:flex md:gap-6 md:items-end">
-                <div className="password flex flex-col gap-2  flex-1">
-                  <Lable label="Choose membership type" />
-                  <select
-                    value={membershipType}
-                    onChange={(e) => {
-                      setMembershipType(e.target.value);
-                      //reset error when membership type chnages
-                      setError((prev) => ({ ...prev, valueError: "" }));
-                    }}
-                    className="px-3 uppercase h-12 outline-[#90BFE9] rounded-lg border border-[#515F69] bg-[#F4F6F7] font-[#A6B4BA]"
-                  >
-                    <option className=" uppercase">
-                      --Select membership type--
-                    </option>
-                    <option className=" uppercase">Life membership</option>
-                    <option className=" uppercase">Annual membership</option>
-                  </select>
-                  {/* <span className="text-sm text-red-400">
+          {membershipItem?.membershipType !== "Life membership" &&
+            currentUser?.user.membershipType !== "Honourary membership" && (
+              <div className=" p-6 bg-white rounded-md shadow-sm transition-all max-w-[512px] mt-6">
+                <h3 className="text-lg font-bold">
+                  <strong>Membership Dues</strong>, Pay online
+                </h3>
+                <p className="mb-6">
+                  Pay for your membership fee online secure and fast
+                </p>
+                <div className="  md:flex md:gap-6 md:items-end">
+                  <div className="password flex flex-col gap-2  flex-1">
+                    <Lable label="Choose membership type" />
+                    <select
+                      value={membershipType}
+                      onChange={(e) => {
+                        setMembershipType(e.target.value);
+                        //reset error when membership type chnages
+                        setError((prev) => ({ ...prev, valueError: "" }));
+                      }}
+                      className="px-3 uppercase h-12 outline-[#90BFE9] rounded-lg border border-[#515F69] bg-[#F4F6F7] font-[#A6B4BA]"
+                    >
+                      <option className=" uppercase">
+                        --Select membership type--
+                      </option>
+                      <option className=" uppercase">Life membership</option>
+                      <option className=" uppercase">Annual membership</option>
+                    </select>
+                    {/* <span className="text-sm text-red-400">
                 {errors.password && touched.password && errors.password}
               </span> */}
-                </div>
+                  </div>
 
-                {/* <Button
+                  {/* <Button
                 handleClick={handleMmebershipPayment}
                 text="Membership Dues"
                 className="px-6 mt-10 md:mt-0 hover:bg-[#1a4f83ec] transition-colors text-slate-50"
               /> */}
-                <Button
-                  handleClick={handleMembershipPayment}
-                  text="Membership Dues"
-                  className="px-6 mt-10 md:mt-0 hover:bg-[#1a4f83ec] flex-1 transition-colors text-slate-50"
-                  isSubmitting={isSubmittingMembershipPayment}
-                  disableBtn={isSubmittingMembershipPayment}
-                >
-                  {isSubmittingMembershipPayment && (
-                    <>
-                      <Oval
-                        visible={true}
-                        height="24"
-                        width="24"
-                        color="#4fa94d"
-                        ariaLabel="oval-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                      />{" "}
-                      <span className="ml-2 text-xs">Loading...</span>
-                    </>
-                  )}
-                </Button>
+                  <Button
+                    handleClick={handleMembershipPayment}
+                    text="Membership Dues"
+                    className="px-6 mt-10 md:mt-0 hover:bg-[#1a4f83ec] flex-1 transition-colors text-slate-50"
+                    isSubmitting={isSubmittingMembershipPayment}
+                    disableBtn={isSubmittingMembershipPayment}
+                  >
+                    {isSubmittingMembershipPayment && (
+                      <>
+                        <Oval
+                          visible={true}
+                          height="24"
+                          width="24"
+                          color="#4fa94d"
+                          ariaLabel="oval-loading"
+                          wrapperStyle={{}}
+                          wrapperClass=""
+                        />{" "}
+                        <span className="ml-2 text-xs">Loading...</span>
+                      </>
+                    )}
+                  </Button>
+                </div>
+                {error.valueError && (
+                  <p className="text-red-400 text-sm mt-2">
+                    {error.valueError}
+                  </p>
+                )}
               </div>
-              {error.valueError && (
-                <p className="text-red-400 text-sm mt-2">{error.valueError}</p>
-              )}
-            </div>
-          )}
+            )}
 
           <div className=" p-6 bg-white rounded-md shadow-sm transition-all max-w-[512px] mt-6">
             <h3 className="text-lg font-bold">
