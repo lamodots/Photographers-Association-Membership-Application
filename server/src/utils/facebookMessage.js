73913b527cfc45045920ff952d901c70bf212fcc
lamodots <lamodots@gmail.com>
2025-04-16 04:22:09 -0700

    Feature: Completed implementation of Whatsapp message integration

--- server/src/utils/facebookMessage.js
+++ server/src/utils/facebookMessage.js
@@ -1,30 +1,32 @@
-/***
- * @function removeFirstZero()
- * @param phoneNumber
- * Example usage:
- * const phoneNumber = "07060980876";
- * const updatedNumber = removeFirstZero(phoneNumber);
- * console.log(updatedNumber); Output: "7060980876"
- *
- */
-function removeFirstZero(phoneNumber) {
-  // Check if the phone number starts with '0'
-  if (phoneNumber.startsWith("0")) {
-    // Remove the first character (the zero)
-    return phoneNumber.slice(1);
-  }
-  // Return the phone number unchanged if it doesn't start with '0'
-  return phoneNumber;
-}
+// /***
+//  * @function removeFirstZero()
+//  * @param phoneNumber
+//  * Example usage:
+//  * const phoneNumber = "07060980876";
+//  * const updatedNumber = removeFirstZero(phoneNumber);
+//  * console.log(updatedNumber); Output: "7060980876"
+//  *
+//  */
+// function removeFirstZero(phoneNumber) {
+//   // Check if the phone number starts with '0'
+//   if (phoneNumber.startsWith("0")) {
+//     // Remove the first character (the zero)
+//     return phoneNumber.slice(1);
+//   }
+//   // Return the phone number unchanged if it doesn't start with '0'
+//   return phoneNumber;
+// }
 
 export async function sendWhatsMessage(whatsappId, membername, memberId) {
+  console.log(whatsappId, membername, memberId);
   //   const userwhatspp = removeFirstZero(whatsappId);
   const res = await fetch(
     `https://graph.facebook.com/v22.0/528735956997502/messages`,
     {
       method: "POST",
       headers: {
-        Authorization: `Bearer EAAP6Cxx5ZBvQBO7ke5jl2ZCYi0Uqr1gao8QYJEAKHSPUYVPHFEGc8rX6cZBmXt5HHPR60jZC2kYh0YXL9wyXc2HiEw7A4ZCNzTgTAjjoZB8Xqh8IFZAQ6q6PKRZAMJv0woQmYeyi1IVvCujWAPpVcDg2Vz9CbNNrdk4ReNJi0s1V3H4oMvLoOLZALdvrRYyYYGJt5Dx0c2FnAmWzoEMlF4XeggOpZCVX1E7y0kSlnvcbfNZAp4ZD`,
+        Authorization: `Bearer ${process.env.KSN_FB_ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         messaging_product: "whatsapp",
@@ -56,7 +58,10 @@ export async function sendWhatsMessage(whatsappId, membername, memberId) {
   );
 
   if (!res.ok) {
-    console.log("Error sending WhatsApp message");
+    const errorResponse = await res.json();
+    console.error("Error sending WhatsApp message:", errorResponse);
+    return; // Optionally return to prevent further execution
   }
-  console.log(res.messages);
+  const responseBody = await res.json();
+  console.log(responseBody);
 }
