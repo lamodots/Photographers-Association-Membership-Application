ad5e6dc37d99a88b5d5db30eae9bfe89fb99a3d8
lamodots <lamodots@gmail.com>
2025-04-17 15:09:13 -0700

    Fix : Refactored the Logout controlller to have new Date(0) to expire the cookie immedialely

--- server/src/controllers/userAuthController.js
+++ server/src/controllers/userAuthController.js
@@ -169,7 +169,7 @@ const completeOnboarding = async (req, res) => {
 const logOutUserController = async (req, res) => {
   res.cookie("token", "logout", {
     httpOnly: true,
-    expires: new Date(Date.now()),
+    expires: new Date(0),
     secure: process.env.NODE_ENV === "production", // Ensure secure cookies in production
     sameSite: "None", // Allow cross-origin cookies
   });
