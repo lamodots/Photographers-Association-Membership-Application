f49560ed26cb1700fb9138bfbeb800f10258e489
lamodots <lamodots@gmail.com>
2025-05-01 07:45:14 -0700

    add PATCH in cors

--- server/src/app.js
+++ server/src/app.js
@@ -59,7 +59,7 @@ const corsOptions = {
     "https://www.ksn.membersng.com",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
